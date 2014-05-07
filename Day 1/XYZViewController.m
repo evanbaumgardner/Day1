80089f96a59923951bd5ccf82652de2a575b0743
Evan Baumgardner <baumer@Goliath.local>
2014-05-06 21:02:19 -0700

    I did some coding?

--- Day 1/XYZViewController.m
+++ Day 1/XYZViewController.m
@@ -14,6 +14,14 @@
 
 @implementation XYZViewController
 
+- (IBAction)buttonPressed:(id)sender
+{
+    
+    NSLog(@"Hello World");
+    
+    self.view.backgroundColor = [UIColor redColor];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -25,5 +33,21 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+-(IBAction)logToConsole:(id)sender
+{
+    UIButton *theButton = sender;
+    NSLog(@"Button Title is: %@", theButton.titleLabel.text);
+    
+    
+}
 
+- (IBAction)logFirstNameToConsole:(id)sender
+{
+    NSLog(@"Evan");
+    
+    UIColor *greenColor = [UIColor greenColor];
+    
+    self.view.backgroundColor = greenColor;
+    
+}
 @end
